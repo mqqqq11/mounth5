75392ef25fa3e5930213adf2de98be4d1677fd00
Azamat <azamustapakylov66@gmail.com>
2023-11-13 23:06:17 +0600

    добавил посты

--- DZ/DZ1/src/pages/CreatePost.jsx
+++ DZ/DZ1/src/pages/CreatePost.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react"
 
 
 export const CreatePost = () => {
+  const [title, setTitle] = useState('')
+  const [storages, setStorages] = useState([])
+
+  const create = () => {
+    const newPost = {id: Date.now(), title}
+    setStorages([...storages, newPost]) 
+
+    setTitle('')
+  }
+  
   return (
-    <div>CreatePost</div>
+    <>
+      <form>
+        <label>
+          <input type="text" value={title} onChange={e => setTitle(e.target.value)}/>
+        </label>
+        <button type="button" onClick={create}>Create</button>
+      </form>
+
+      <ul>
+        {storages.map(storage => (
+          <li key={storage.id}>{storage.title}</li>
+        ))}
+      </ul>
+    </>
   )
 }
--- DZ/DZ1/src/pages/PostsPage.jsx
+++ DZ/DZ1/src/pages/PostsPage.jsx
@@ -1,7 +1,24 @@
+import { useEffect } from "react"
+import { useState } from "react"
 
 
 export const PostsPage = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() =>{
+        fetch('https://dummyjson.com/posts')
+        .then(responce => responce.json())
+        .then(json => setPosts(json.posts))
+    }, [])
   return (
-    <div>PostsPage</div>
+    <>
+        <div>PostsPage
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
+        </div>
+    </>
   )
 }
